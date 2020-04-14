9186f55f2bebc6ad928b9f13f73fc9a9eeeeb8fa
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 17:34:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Add your code here
+class Dog
+
+  attr_accessor :name
+
+  @@all = [] 
+
+  def initialize(name)
+    @name = name 
+    @@all < self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.print_all
+    @@all.each{|dog| puts dog.name}
+  end
+
+end
